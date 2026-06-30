3361b5df10f92f544d32ad181a980e15416dbf4f
Gaurav Sharma <gauravsharmaa0111@gmail.com>
2026-06-30 12:36:32 +0530

    chnaged localhost to domain

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -9,14 +9,11 @@ import { useAuth } from '../context/AuthContext';
 
 // export const baseUrl = 'http://192.168.1.52:6969';
 
-// export const baseUrl = 'https://backend.nftrealestate.us';
-// export const imgBaseUrl = 'https://backend.nftrealestate.us';
+// export const baseUrl = 'http://localhost:5000';
+// export const imgBaseUrl = 'http://localhost:5000';
 
-// export const baseUrl = 'http://api.nftrealestate.genzteck.com';
-// export const imgBaseUrl = 'http://api.nftrealestate.genzteck.com';
-
-export const baseUrl = 'http://localhost:5000';
-export const imgBaseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://node.nftrealestate.us';
+export const imgBaseUrl = 'https://node.nftrealestate.us';
 
 
 
